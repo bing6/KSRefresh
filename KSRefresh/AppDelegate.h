a164fb9f5af6a054fc2c02975ee67f3fcacefcf5
bing.hao <bing.hao1982@gmail.com>
2015-04-04 12:07:31 +0800

    Initial Commit

---
+++ KSRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSRefresh
+//
+//  Created by bing.hao on 15/4/4.
+//  Copyright (c) 2015年 bing.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KSRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSRefresh
+//
+//  Created by bing.hao on 15/4/4.
+//  Copyright (c) 2015年 bing.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
